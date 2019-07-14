0a7c34bf3791dff312844e74d0001af48fd2bf62
Jon Shern <jonshern@outlook.com>
2019-07-14 10:46:29 -0500

    still blinking

--- pingpong.ino
+++ pingpong.ino
@@ -1,17 +1,13 @@
-
-// First Task : Button Test
-// Get the Button turning on and off the built in LED
-
-void setup()
-{
-    // initialize digital pin LED_BUILTIN as an output.
-    pinMode(LED_BUILTIN, OUTPUT);
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop()
-{
+// the loop function runs over and over again forever
+void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(3000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(3000); 
+  delay(3000);                       // wait for a second
 }
